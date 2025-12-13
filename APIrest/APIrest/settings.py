4b68da8b97caa6da19c3dfc9109cf478e1fac008
locosaravia <alvarosaraviag@gmail.com>
2025-12-13 19:34:59 -0300

    agrego algo

--- APIrest/APIrest/settings.py
+++ APIrest/APIrest/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ==================== REST FRAMEWORK CONFIGURATION ====================
-
+# aaaaa
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
